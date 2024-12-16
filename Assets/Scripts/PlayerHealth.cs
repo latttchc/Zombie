92a4cd591fdd6f41200ef43adb144f7b299d0e68
latttchc <iam.yuk2882@gmail.com>
2024-12-16 18:09:42 +0900

    ZG-04

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,40 +1,84 @@
 ﻿using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
-public class PlayerHealth : LivingEntity {
-    public Slider healthSlider; 
+public class PlayerHealth : LivingEntity
+{
+    public Slider healthSlider;
 
-    public AudioClip deathClip; 
-    public AudioClip hitClip; 
-    public AudioClip itemPickupClip; 
+    public AudioClip deathClip;
+    public AudioClip hitClip;
+    public AudioClip itemPickupClip;
 
-    private AudioSource playerAudioPlayer; 
-    private Animator playerAnimator; 
+    private AudioSource playerAudioPlayer;
+    private Animator playerAnimator;
 
-    private PlayerMovement playerMovement; 
-    private PlayerShooter playerShooter; 
+    private PlayerMovement playerMovement;
+    private PlayerShooter playerShooter;
 
-    private void Awake() {
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerAudioPlayer = GetComponent<AudioSource>();
 
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooter = GetComponent<PlayerShooter>();
     }
 
-    protected override void OnEnable() {
+    protected override void OnEnable()
+    {
         base.OnEnable();
+
+        healthSlider.gameObject.SetActive(true);
+        healthSlider.maxValue = startingHealth;
+        healthSlider.value = health;
+
+        playerMovement.enabled = true;
+        playerShooter.enabled = true;
     }
 
-    public override void RestoreHealth(float newHealth) {
+    public override void RestoreHealth(float newHealth)
+    {
         base.RestoreHealth(newHealth);
+
+        healthSlider.value = health;
     }
 
-    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection) {
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection)
+    {
+        if (!dead)
+        {
+            playerAudioPlayer.PlayOneShot(hitClip);
+        }
+
         base.OnDamage(damage, hitPoint, hitDirection);
+
+        healthSlider.value = health;
     }
 
-    public override void Die() {
+    public override void Die()
+    {
         base.Die();
+
+        healthSlider.gameObject.SetActive(false);
+
+        playerAudioPlayer.PlayOneShot(deathClip);
+
+        playerAnimator.SetTrigger("Die");
+
+        playerMovement.enabled = false;
+        playerShooter.enabled = false;
     }
 
-    private void OnTriggerEnter(Collider other) {
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!dead)
+        {
+            IItem item = other.GetComponent<IItem>();
+            if (item != null)
+            {
+                item.Use(gameObject);
+                playerAudioPlayer.PlayOneShot(itemPickupClip);
+            }
+        }
     }
 }
