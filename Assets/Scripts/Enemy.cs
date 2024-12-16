cd52b991bbeebd8917c7a9fe7a04a814e7f1bf2f
latttchc <iam.yuk2882@gmail.com>
2024-12-16 19:21:44 +0900

    ZG-05

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,25 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.AI; 
+using UnityEngine.AI;
 
-public class Enemy : LivingEntity {
-    public LayerMask whatIsTarget; 
+public class Enemy : LivingEntity
+{
+    public LayerMask whatIsTarget;
 
-    private LivingEntity targetEntity; 
-    private NavMeshAgent pathFinder; 
+    private LivingEntity targetEntity;
+    private NavMeshAgent pathFinder;
 
-    public ParticleSystem hitEffect; 
-    public AudioClip deathSound; 
-    public AudioClip hitSound; 
+    public ParticleSystem hitEffect;
+    public AudioClip deathSound;
+    public AudioClip hitSound;
 
-    private Animator enemyAnimator; 
-    private AudioSource enemyAudioPlayer; 
-    private Renderer enemyRenderer; 
+    private Animator enemyAnimator;
+    private AudioSource enemyAudioPlayer;
+    private Renderer enemyRenderer;
 
-    public float damage = 20f; 
-    public float timeBetAttack = 0.5f; 
-    private float lastAttackTime; 
+    public float damage = 20f;
+    public float timeBetAttack = 0.5f;
+    private float lastAttackTime;
 
     private bool hasTarget
     {
@@ -33,37 +34,112 @@ public class Enemy : LivingEntity {
         }
     }
 
-    private void Awake() {
+    private void Awake()
+    {
+        pathFinder = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        enemyAudioPlayer = GetComponent<AudioSource>();
+
+        enemyRenderer = GetComponentInChildren<Renderer>();
 
     }
 
-    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor) {
+    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor)
+    {
+        startingHealth = newHealth;
+        health = newHealth;
+
+        damage = newDamage;
+
+        pathFinder.speed = newSpeed;
+
+        enemyRenderer.material.color = skinColor;
     }
 
-    private void Start() {
+    private void Start()
+    {
         StartCoroutine(UpdatePath());
     }
 
-    private void Update() {
+    private void Update()
+    {
         enemyAnimator.SetBool("HasTarget", hasTarget);
     }
 
-    private IEnumerator UpdatePath() {
+    private IEnumerator UpdatePath()
+    {
         while (!dead)
         {
+            if (hasTarget)
+            {
+                pathFinder.isStopped = false;
+                pathFinder.SetDestination(targetEntity.transform.position);
+            }
+            else
+            {
+                pathFinder.isStopped = true;
+
+                Collider[] colliders = Physics.OverlapSphere(transform.position, 20f, whatIsTarget);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    LivingEntity livingEntity = colliders[i].GetComponent<LivingEntity>();
+
+                    if (livingEntity != null && !livingEntity.dead)
+                    {
+                        targetEntity = livingEntity;
+
+                        break;
+                    }
+                }
+            }
             yield return new WaitForSeconds(0.25f);
         }
     }
 
-    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal) {
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        if (!dead)
+        {
+            hitEffect.transform.position = hitPoint;
+            hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+            hitEffect.Play();
+
+            enemyAudioPlayer.PlayOneShot(hitSound);
+        }
         base.OnDamage(damage, hitPoint, hitNormal);
     }
 
-    public override void Die() {
+    public override void Die()
+    {
         base.Die();
+
+        Collider[] enemyColliders = GetComponents<Collider>();
+        for (int i = 0; i < enemyColliders.Length; i++)
+        {
+            enemyColliders[i].enabled = false;
+        }
+        pathFinder.isStopped = true;
+        pathFinder.enabled = false;
+
+        enemyAnimator.SetTrigger("Die");
+        enemyAudioPlayer.PlayOneShot(deathSound);
     }
 
-    private void OnTriggerStay(Collider other) {
+    private void OnTriggerStay(Collider other)
+    {
+        if (!dead && Time.time > lastAttackTime + timeBetAttack)
+        {
+            LivingEntity attackTarget = other.GetComponent<LivingEntity>();
 
+            if (attackTarget != null && attackTarget == targetEntity)
+            {
+                lastAttackTime = Time.time;
+                Vector3 hitPoint = other.ClosestPoint(transform.position);
+                Vector3 hitNomal = transform.position - other.transform.position;
+
+                attackTarget.OnDamage(damage, hitPoint, hitNomal);
+            }
+        }
     }
 }
